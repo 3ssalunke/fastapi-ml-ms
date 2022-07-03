2d0fbc8479918203cab7aaa582ab81efea092ca8
surajs <3ssalunke>
2022-07-03 22:50:38 +0530

    added pyTest

--- app/main.py
+++ app/main.py
@@ -11,3 +11,7 @@ templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 @app.get("/", response_class=HTMLResponse)
 def home_view(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
+
+@app.post("/")
+def home_detail_view(request: Request):
+    return {"message": "success"}
