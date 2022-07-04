ae89a1d9a7b4c0dfdbfa7bed29e5a0878a15258a
surajs <3ssalunke>
2022-07-05 00:26:50 +0530

    add image uplaod with tests

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 import pathlib
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+import io
+import uuid
+from fastapi import (
+    FastAPI, 
+    Request,
+    File,
+    UploadFile,
+    HTTPException
+)
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
+from PIL import Image
 
 BASE_DIR = pathlib.Path(__file__).parent
+UPLOAD_DIR = BASE_DIR / "uploaded"
 
 app = FastAPI()
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
@@ -15,3 +25,20 @@ def home_view(request: Request):
 @app.post("/")
 def home_detail_view(request: Request):
     return {"message": "success"}
+
+@app.post("/img-echo", response_class=FileResponse)
+async def img_echo_view(file: UploadFile = File(...)):
+    UPLOAD_DIR.mkdir(exist_ok=True)
+    bytes_str = io.BytesIO(await file.read())
+    try:
+        img = Image.open(bytes_str)
+    except:
+        raise HTTPException(detail="Invalid image", status_code=400)
+    fname = pathlib.Path(file.filename)
+    fext = fname.suffix
+    dest = UPLOAD_DIR / f"{uuid.uuid1()}{fext}"
+    # with open(str(dest), 'wb') as out:
+    #     out.write(bytes_str.read())
+    img.save(dest)
+    
+    return dest
--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -1,5 +1,9 @@
+import shutil
+import time
+import io
 from fastapi.testclient import TestClient
-from app.main import app
+from PIL import Image, ImageChops
+from app.main import app, BASE_DIR, UPLOAD_DIR
 
 client = TestClient(app)
 
@@ -13,4 +17,28 @@ def test_post_home():
     response = client.post("/")
     assert response.status_code == 200
     assert "application/json" in response.headers["content-type"]
+
+
+
+def test_post_img_echo():
+    image_saved_path = BASE_DIR / "images"
+    for path in image_saved_path.glob("*"):
+        try:
+            img = Image.open(path)
+        except:
+            img = None
+        response = client.post("/img-echo", files={"file": open(path, 'rb')})
+        fext = str(path.suffix).replace(".", "")
+        if img is None:
+            assert response.status_code == 400
+        else:
+            assert response.status_code == 200
+            r_stream = io.BytesIO(response.content)
+            echo_img = Image.open(r_stream)
+            difference = ImageChops.difference(img, echo_img).getbbox()
+            assert difference is None
+
+    time.sleep(3)
+    shutil.rmtree(UPLOAD_DIR)
+    
     
